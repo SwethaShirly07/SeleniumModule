ad1e4599b406b6f98475e94e9519b2fa444ea036
SwethaShirly07 <swethashirly7@gmail.com>
2024-04-05 09:29:31 +0530

    my second code sample push is ready

--- src/test/java/Java_Programs/Day2.java
+++ src/test/java/Java_Programs/Day2.java
@@ -1,11 +1,8 @@
 package Java_Programs;
 
-import java.sql.SQLOutput;
-
 public class Day2 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-        System.out.println("Sample");
+        System.out.println("Swetha second code push");
     }
-
 }
